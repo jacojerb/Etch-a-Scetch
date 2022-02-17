3d13aeca021874f41ab67938976e483fdb68aa9d
jacojerb <jacojerb@gmail.com>
2022-02-17 17:44:20 +0200

    updated the UI and added options for fade to black and random colours

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let gridStyle = "classic"
+
+let classicButton = document.querySelector(".blackStyle")
+classicButton.addEventListener("click", function() {
+    gridStyle = "classic"
+})
+let fadeButton = document.querySelector(".fadeStyle")
+fadeButton.addEventListener("click", function() {
+    gridStyle = "fade"
+})
+let randomButton = document.querySelector(".randomColour")
+randomButton.addEventListener("click", function() {
+    gridStyle = "random"
+})
+
 function createGrid(size) {
     if (size > 100) {
         alert("error: Maximum grid size:100")
@@ -12,8 +27,27 @@ function createGrid(size) {
             let innerDiv = document.createElement(`div`);
             innerDiv.classList.add("horizontal");
             div.appendChild(innerDiv);
+            innerDiv.style.backgroundColor = "hsl(0,0%,100%)"
             innerDiv.addEventListener("mouseover", function() {
-                innerDiv.style.backgroundColor = "black"
+              if (gridStyle == "classic") { innerDiv.style.backgroundColor = "black"}
+              else if (gridStyle == "fade") { 
+                let rgb = innerDiv.style.backgroundColor.slice(4,7);
+                let darkness = parseInt(rgb, 10);
+                let newDarkness;
+                if (darkness < 26) {
+                    newDarkness = 0;
+                } else {
+                    newDarkness = darkness - 26;
+                } 
+                innerDiv.style.backgroundColor = "rgb(" + newDarkness + "," + newDarkness + "," + newDarkness + ")"
+
+
+              } else {
+                  let random1 = Math.random() * 255;
+                  let random2 = Math.random() * 255;
+                  let random3 = Math.random() * 255;
+                  innerDiv.style.backgroundColor = "rgb(" + random1 + "," + random2 + "," + random3 + ")"
+              }
             })
         }
     }
